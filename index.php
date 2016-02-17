f1e5e342c2beba081916a2cb52d3d7dc9e239d45
Baptiste <baptiste@lyondev.fr>
2016-02-18 00:01:11 +0100

    modification page d'accueil pour acceder aux differents liens plus facilement

--- index.php
+++ index.php
@@ -2,40 +2,53 @@
 <head>
     <title>Maets</title>
 	<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.8/jquery.min.js"></script>
+	
+<meta charset="UTF-8">
+
+
+<style media="screen" type="text/css">
+
+.parent div{
+	float: left;
+	clear: none;
+}
+
+table
+
+{
+    border-collapse: collapse; 
+}
+
+td
+{
+    border: 1px solid black;
+	padding: 4px;
+}
+th
+{
+	padding: 4px;
+}
+
+</style>
+
 
 </head>
 <body>
-    <input class ="btn btn-primary" type="submit" onclick="soap()">
-    <?php
-
-      $data = array();
-      $data = json_decode(file_get_contents("php://input"), true);
-      var_dump($data);
-    ?>
-    <div id='result'>
+<h1><a href="index.php">Maets</a></h1>
+<br/><br/>
+<a href="joueur.php"> Rechercher un joueur </a><br/>
+<a href="jeux.php"> Rechercher un jeu </a>
     
-    </div>
-    <script>
-    function soap() {
-    	var message = {
-    		player: 'John',
-    		badge: 'Coucou'
-    	}
-        $.ajax({
+    
+    
+    
 
-            url : '/stats/Lol',
-            
-            //data: 'data= ' + $getValue.value,
-            data: message,
-            type : 'PUT',
-            dataType : 'text', // On désire recevoir du HTML
-            success : function(message, code_html, statut){ // code_html contient le HTML renvoyé
-               //alert('toto');
-               //document.location.href="/stats/Lol"
-                $('#result').text(message);
-            }
-         });
-    }
-    </script>
+    
+    
+<script>
+
+    
+</script>
+    
 </body>
 </html>
